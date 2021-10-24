fa96c96c3f43caac68cb5b9574ec4a2d7870d1f1
Sébastien Thuret <contact@sebastien-thuret.fr>
2021-10-24 11:55:53 +0200

    fix tests

--- tests/test_init.py
+++ tests/test_init.py
@@ -4,6 +4,6 @@ from argostranslate import package
 
 def test_boot_argos():
     """Test Argos translate models initialization"""
-    argostranslate.argospm.install_package("translate-en_fr")
+    argostranslate.argospm.install_package({"name": "translate-en_fr"})
 
     assert len(package.get_installed_packages()) == 1
