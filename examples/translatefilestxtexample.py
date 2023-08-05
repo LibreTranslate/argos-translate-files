7954e67b6420e758c050aa36aa567d66dbe065e6
P.J. Finlay <pjappdevelopment@gmail.com>
2023-08-05 09:40:11 -0500

    Improve the examples script
    Accept command line parameters in the example script
    
    Add example epub, html, odt, and txt files for easy testing

---
+++ examples/translatefilestxtexample.py
@@ -0,0 +1,26 @@
+import os.path
+import argparse
+
+from argostranslate import translate
+
+from argostranslatefiles import argostranslatefiles
+
+"""
+# Usage
+
+python3 examples/translatefilestxtexample.py examples/example.epub
+python3 examples/translatefilestxtexample.py examples/example.txt
+python3 examples/translatefilestxtexample.py examples/example.odt
+python3 examples/translatefilestxtexample.py examples/example.html
+
+"""
+
+# Parse command line args
+parser = argparse.ArgumentParser(description='Translate a file.')
+parser.add_argument('file_path', type=str, help='Path to file to translate')
+args = parser.parse_args()
+
+# Translate file
+installed_languages = translate.get_installed_languages()
+underlying_translation = installed_languages[0].get_translation(installed_languages[1])
+print(argostranslatefiles.translate_file(underlying_translation, os.path.abspath(args.file_path)))
